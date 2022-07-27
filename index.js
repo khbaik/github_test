50f9515f16bb31da3ee615a721c932ac21592f53
Kyeonghoon Baik <>
2022-07-27 21:13:32 +0900

    init first version web server

---
+++ index.js
@@ -0,0 +1,16 @@
+import Fastify from 'fastify'
+
+const fastify = Fastify({logger: true})
+
+fastify.get('/', function(request, reply) {
+    reply.send({hello: 'world'})
+})
+
+fastify.listen({port: 4040}, function (err, address) {
+    if (err) {
+        fastify.log.error(err)
+        process.exit(1)
+    }
+
+    console.log(`${address}`)
+})
